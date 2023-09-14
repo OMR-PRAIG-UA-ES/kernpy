d4b77dc1f0478a6e842d0f04ac2b8d65cdf1db24
davidrizo <drizovalero@gmail.com>
2023-09-14 22:22:27 +0200

    First example

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Python Humdrum **kern and **mens utilities
+It requires the antlr4 package to be installed using:
+```shell
+pip install antlr4-python3-runtime
+```
---
+++ test/test_importer.py
@@ -0,0 +1,14 @@
+import unittest
+import pytest
+from src.importer import KernImporter
+
+class ImporterTestCase(unittest.TestCase):
+    # it loads a simple JSON file
+    def testReadMinimalKern(self):
+        importer = KernImporter()
+        importer.doImportFile('resource_dir/minimal.json')
+        # self.assertEqual(1, len(ts.files))
+
+
+if __name__ == '__main__':
+    unittest.main()
