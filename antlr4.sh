a85907950a623a11efa65ec0ebe330f0589ea09d
davidrizo <drizovalero@gmail.com>
2023-09-14 21:19:22 +0200

    Folders changed

--- antlr4.sh
+++ antlr4.sh
@@ -1,3 +1,3 @@
-java -jar ./antlr-4.13.0-complete.jar grammar/kernLexer.g4 -Dlanguage=Python3 -o src/generated/antlr
-cp src/generated/antlr/grammar/kernLexer.tokens grammar
-java -jar ./antlr-4.13.0-complete.jar -visitor grammar/kernParser.g4  -Dlanguage=Python3 -o src/generated/antlr
+java -jar ./antlr-4.13.0-complete.jar kern/kernLexer.g4 -Dlanguage=Python3 -o src/generated
+cp src/generated/kern/kernLexer.tokens kern
+java -jar ./antlr-4.13.0-complete.jar -visitor kern/kernParser.g4  -Dlanguage=Python3 -o src/generated
