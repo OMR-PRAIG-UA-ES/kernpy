7bf34eb31c422154c1d875b0fb59780b545a5cca
jcerveto <joan.cerveto.serrano@gmail.com>
2024-03-20 11:34:57 +0100

    Skip not implemented tests

--- test/test_generic.py
+++ test/test_generic.py
@@ -6,43 +6,32 @@ import sys
 import kernpy
 
 class GenericTestCase(unittest.TestCase):
+    @unittest.skip("Not implemented")
     def test_measure_from_time(self):
-        self.assertTrue(True)
-        return
-
         score = kernpy.read_kern('path/to/file.krn')
         measure = score.measure_from_time(10)
         self.assertTrue(measure > 0)
 
+    @unittest.skip
     def test_extract_spine(self):
-        self.assertTrue(True)
-        return
-
         score = kernpy.read_kern('path/to/file.krn')
         spine = score.extract_spine(0)
         self.assertTrue(spine)
 
+    @unittest.skip
     def test_extract_measures(self):
-        self.assertTrue(True)
-        return
-
-
         score = kernpy.read_kern('path/to/file.krn')
         measures = score.extract_measures(0, 1)
         self.assertTrue(measures)
 
+    @unittest.skip
     def test_tokens(self):
-        self.assertTrue(True)
-        return
-
         score = kernpy.read_kern('path/to/file.krn')
         tokens = score.tokens()
         self.assertTrue(tokens)
 
+    @unittest.skip
     def test_to_krn(self):
-        self.assertTrue(True)
-        return
-
         score = kernpy.read_kern('path/to/file.krn')
         score.to_krn()
         self.assertTrue(os.path.exists('path/to/file.krn'))
