0f2e3ed4802380075a6a9bf2767b98e762f720a1
jcerveto <joan.cerveto.serrano@gmail.com>
2024-04-16 12:15:49 +0200

    Render filtered

--- renderKern.py
+++ renderKern.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import random
 
 import numpy as np
 import multiprocessing
@@ -12,6 +13,7 @@ import cv2
 ### GLOBAL VARIABLES ###
 PROCESSES_PARALLEL = 8
 input_dir = ''
+log_file = ''
 
 
 def add_log(msg: str, is_correct: bool) -> None:
@@ -27,6 +29,32 @@ def add_log(msg: str, is_correct: bool) -> None:
         f.write(f'{msg.replace(SEPARATOR, ";")}{SEPARATOR}{status}\n')
 
 
+def find_image_cut(sample, margin_bot=40, margin_right=40):
+    cut_height = None
+    cut_width = None
+
+    # Get the height and width of the image
+    height, width = sample.shape[:2]
+
+    # Iterate through the image array from the bottom to the top
+    for y in range(height - 1, -1, -1):
+        if [0, 0, 0] in sample[y]:
+            cut_height = y + margin_bot
+            break
+
+    for x in range(width - 1, -1, -1):
+        if [0, 0, 0] in sample[:, x]:
+            cut_width = x + margin_right
+            break
+
+    # If no black pixel is found, return None
+    return cut_height, cut_width
+
+
+def rfloat(start, end):
+    return round(random.uniform(start, end), 2)
+
+
 def render_image(args) -> None:
     """
     Render the image for the given importer and save it in the given folder
@@ -34,29 +62,46 @@ def render_image(args) -> None:
     """
     input_file, progress_queue = args
     try:
+        input_file, progress_queue = args
         # Redirect stdout and stderr
-        #sys.stdout = open(os.devnull, 'w')
-        #sys.stderr = open(os.devnull, 'w')
-
-        new_file_path_png = input_file.replace('.krn', '.png')
-        print(f'Creating file: {new_file_path_png}')
+        sys.stdout = open(os.devnull, 'w')
+        sys.stderr = open(os.devnull, 'w')
 
         # Create a verovio toolkit instance
         vtk = verovio.toolkit()
 
+        # Set rendering options
+        width = random.choice([2100, 3000])
+        vtk.setOptions({
+            "pageWidth": width,
+            "footer": 'none',
+            'barLineWidth': rfloat(0.3, 0.8),
+            'beamMaxSlope': rfloat(10, 20),
+            'staffLineWidth': rfloat(0.1, 0.3),
+            'spacingStaff': rfloat(4, 12)
+        })
+
         # Load the file
         vtk.loadFile(input_file)
 
-        # Render the file
+        # Render the file to SVG
         svg_content = vtk.renderToSVG()
         svg_content = svg_content.replace("overflow=\"inherit\"", "overflow=\"visible\"")
 
         # Convert the SVG to PNG
-        png_content = svg2png(bytestring=svg_content, background_color='white')
+        png_content = svg2png(bytestring=svg_content, background_color='white', dpi=300)
         png_data = np.frombuffer(png_content, np.uint8)
         png_img = cv2.imdecode(png_data, cv2.IMREAD_UNCHANGED)
 
+        # Cut white boxes from the image
+        cut_height, cut_width = find_image_cut(png_img)
+        if cut_height is not None:
+            png_img = png_img[:cut_height, :]
+        if cut_width is not None:
+            png_img = png_img[:, :cut_width]
+
         # Save the PNG
+        new_file_path_png = input_file.replace('.krn', '.png')
         cv2.imwrite(new_file_path_png, png_img)
 
         # Update progress
@@ -66,7 +111,6 @@ def render_image(args) -> None:
     except Exception as e:
         add_log(f'{input_file}->{e}', False)
     finally:
-        pass
         # Restore stdout and stderr
         sys.stdout = sys.__stdout__
         sys.stderr = sys.__stderr__
@@ -92,13 +136,14 @@ def generate_rendered_images(input_dir: str, _log_file: str) -> None:
 
     print(clean_input_files)
     # Tasks
-    tasks = list(zip(clean_input_files,))
+    tasks = list(zip(clean_input_files, ))
 
     # Shared progress queue
     manager = multiprocessing.Manager()
     progress_queue = manager.Queue()
     total_tasks = len(tasks)
 
+    print(f'Total tasks: {total_tasks}')
     # Run the tasks
     # Sequential processing
     #for task in tqdm(tasks, desc='Creating fragments'):
