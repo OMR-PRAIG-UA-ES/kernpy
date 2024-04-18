9ec3ec9047af71f3ef51c11a3c77640a1bc337b9
jcerveto <joan.cerveto.serrano@gmail.com>
2024-04-18 13:55:35 +0200

    use __all__ to importing modules

--- kernpy/core/__init__.py
+++ kernpy/core/__init__.py
@@ -6,59 +6,19 @@ kernpy.core
 This module contains the core functionality of the `kernpy` package.
 """
 
-from .import_humdrum import (
-    HumdrumImporter,
-    ExportOptions,
-    ekern_to_krn,
-    kern_to_ekern,
-    KernTypeExporter,
-    TokenCategory
-)
-
-from .tokens import (
-    BEKERN_CATEGORIES,
-)
-
-
-from .dyn_importer import (
-    DynSpineImporter,
-)
-
-from .dynam_spine_importer import (
-    DynamSpineImporter,
-)
-
-from .fing_spine_importer import (
-    FingSpineImporter,
-)
-
-from .harm_spine_importer import (
-    HarmSpineImporter,
-)
-
-from .kern_spine_importer import (
-    KernSpineImporter,
-)
-
-from .mens_spine_importer import (
-    MensSpineImporter,
-)
-
-from .root_importer import (
-    RootSpineImporter,
-)
-
-from .text_spine_importer import (
-    TextSpineImporter,
-)
+from .import_humdrum import *
+from .tokens import *
+from .dyn_importer import *
+from .dynam_spine_importer import *
+from .fing_spine_importer import *
+from .harm_spine_importer import *
+from .kern_spine_importer import *
+from .mens_spine_importer import *
+from .root_importer import *
+from .text_spine_importer import *
+from .generic import *
 
 
-from .generic import (
-    read_kern,
-    Score,
-)
-
-# TODO: Explore using __all__
 __all__ = [
     'HumdrumImporter',
     'ExportOptions',
--- kernpy/polish_scores/__init__.py
+++ kernpy/polish_scores/__init__.py
@@ -6,6 +6,10 @@ polish_scores
 This module provides a way to download and process the Polish Scores dataset.
 """
 
-from .download_polish_dataset import main
+from .download_polish_dataset import *
+
+__all__ = [
+    'main'
+]
 
 
