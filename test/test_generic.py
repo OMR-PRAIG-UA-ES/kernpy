dfad6fb3578cc6d32f9ebcb61552a3408df8520a
jcerveto <joan.cerveto.serrano@gmail.com>
2024-05-28 09:45:12 +0200

    Test public interface

--- test/test_generic.py
+++ test/test_generic.py
@@ -6,4 +6,17 @@ import sys
 import kernpy
 
 class GenericTestCase(unittest.TestCase):
-    pass
+    def test_read_export_easy(self):
+        # Arrange
+        expected_ekrn = 'resource_dir/legacy/base_tuplet.ekrn'
+        current_krn = 'resource_dir/legacy/base_tuplet.krn'
+        with open(expected_ekrn, 'r') as f:
+            expected_content = f.read()
+
+        # Act
+        doc = kernpy.read(current_krn)
+        options = kernpy.ExportOptions(kern_type=kernpy.KernTypeExporter.eKern)
+        real_content = kernpy.export(doc, options)
+
+        # Assert
+        self.assertEqual(expected_content, real_content, f"File content mismatch: \nExpected:\n{expected_content}\n{40 * '='}\nReal\n{real_content}")
