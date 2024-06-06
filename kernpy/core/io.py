9bc6dd77a66698246c45141155aae5aa7c8fd746
jcerveto <joan.cerveto.serrano@gmail.com>
2024-06-06 10:49:07 +0200

    Create the folder using exist_ok=True

--- kernpy/core/io.py
+++ kernpy/core/io.py
@@ -3,7 +3,7 @@ import os
 
 def _write(path, content) -> None:
     if not os.path.exists(os.path.dirname(path)):
-        os.makedirs(os.path.dirname(path))
+        os.makedirs(os.path.dirname(path), exist_ok=True)
 
     with open(path, 'w') as f:
         f.write(content)
