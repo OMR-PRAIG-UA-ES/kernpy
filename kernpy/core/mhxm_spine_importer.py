438313487e7fd3b6cfe4ac4565736dadf8a7e176
jcerveto <joan.cerveto@ua.es>
2024-08-15 17:52:29 +0200

    Add TODO

--- kernpy/core/mhxm_spine_importer.py
+++ kernpy/core/mhxm_spine_importer.py
@@ -5,6 +5,6 @@ from .tokens import MHXMToken, Token
 
 
 class MxhmSpineImporter(SpineImporter):
-    def import_token(self, encoding: string)->Token:
-        return MHXMToken(encoding)
+    def import_token(self, encoding: string) -> Token:
+        return MHXMToken(encoding) # TODO: implement constructor for MHXMToken
 
