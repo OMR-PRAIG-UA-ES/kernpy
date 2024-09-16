ce3d6ae64acf07269a86f68604d0c713475caf3f
jcerveto <joan.cerveto@ua.es>
2024-09-16 11:29:20 +0200

    docs + feat: easy

--- kernpy/core/_io.py
+++ kernpy/core/_io.py
@@ -1,10 +1,46 @@
 import os
+import pathlib
+from typing import Optional
 
 
 def _write(path, content) -> None:
+    """
+    Store content in a file.
+
+    Args:
+        path (str): Path to the file.
+        content (str): Content to be stored in the file.
+
+    Returns: None
+
+    """
     if not os.path.exists(os.path.dirname(path)):
         os.makedirs(os.path.dirname(path), exist_ok=True)
 
     with open(path, 'w') as f:
         f.write(content)
 
+
+def find_all_files(
+        path: str,
+        extension: Optional[str] = None) -> list:
+    """
+    Find all files with the given extension in the given directory.
+    Args:
+        path (str): Path to the directory where the files are located.
+        extension (Optional[str]): Extension of the files to be found. If None, all files are returned.
+
+    Returns (List[str]): List of paths to the found files.
+
+    Examples:
+        >>> find_all_files('/kern_files', 'krn')
+        ...
+
+        >>> find_all_files('/files' )
+        ...
+    """
+    p = pathlib.Path(path)
+    if extension is None:
+        return list(p.glob('**/*'))
+    else:
+        return list(p.glob(f'**/*.{extension}'))
